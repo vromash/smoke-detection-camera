da73543cc6e61833c675a619b6e543e0ebf774a6
Vladislavs Romass <vld.romash@gmail.com>
2021-01-13 02:54:35 +0200

    revert

--- backend/app/api/image.py
+++ backend/app/api/image.py
@@ -15,27 +15,25 @@ class ImageApi(Resource):
     def post(self):
         json_data = request.get_json(force=True)
 
-        return json_data
+        url = json_data['url']
+        print_output = json_data['print_output']
 
-        # url = json_data['url']
-        # print_output = json_data['print_output']
+        image_path = download_image(url)
 
-        # image_path = download_image(url)
+        command = f'./darknet detector test \
+            build/darknet/x64/data/obj.data \
+            cfg/yolov4-tiny-obj.cfg \
+            build/darknet/x64/backup/yolov4-tiny-obj_final.weights { image_path } -i 0 -thresh 0.20'
 
-        # command = f'./darknet detector test \
-        #     build/darknet/x64/data/obj.data \
-        #     cfg/yolov4-tiny-obj.cfg \
-        #     build/darknet/x64/backup/yolov4-tiny-obj_final.weights { image_path } -i 0 -thresh 0.20'
+        process = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE)
+        process_alive = True
+        while process_alive:
+            output = process.stdout.readline()
+            if print_output:
+                print(output)
 
-        # process = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE)
-        # process_alive = True
-        # while process_alive:
-        #     output = process.stdout.readline()
-        #     if print_output:
-        #         print(output)
+            if output == b'' and process.poll() is not None:
+                process_alive = False  
 
-        #     if output == b'' and process.poll() is not None:
-        #         process_alive = False  
-
-        # return send_file(darknet_path + '/predictions.jpg', mimetype='image/jpg')
+        return send_file(darknet_path + '/predictions.jpg', mimetype='image/jpg')
         
