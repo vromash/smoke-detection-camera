82d1f2888a0b63aab71e11335acc28b0b2762fdb
Vladislavs Romass <vld.romash@gmail.com>
2021-01-13 03:06:22 +0200

    daewqad

--- backend/app/api/image.py
+++ backend/app/api/image.py
@@ -20,22 +20,20 @@ class ImageApi(Resource):
 
         image_path = download_image(url)
 
-        return image_path
+        command = f'./content/darknet/darknet detector test \
+            build/darknet/x64/data/obj.data \
+            cfg/yolov4-tiny-obj.cfg \
+            build/darknet/x64/backup/yolov4-tiny-obj_final.weights { image_path } -i 0 -thresh 0.20'
 
-        # command = f'./darknet detector test \
-        #     build/darknet/x64/data/obj.data \
-        #     cfg/yolov4-tiny-obj.cfg \
-        #     build/darknet/x64/backup/yolov4-tiny-obj_final.weights { image_path } -i 0 -thresh 0.20'
+        process = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE)
+        process_alive = True
+        while process_alive:
+            output = process.stdout.readline()
+            if print_output:
+                print(output)
 
-        # process = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE)
-        # process_alive = True
-        # while process_alive:
-        #     output = process.stdout.readline()
-        #     if print_output:
-        #         print(output)
+            if output == b'' and process.poll() is not None:
+                process_alive = False  
 
-        #     if output == b'' and process.poll() is not None:
-        #         process_alive = False  
-
-        # return send_file(darknet_path + '/predictions.jpg', mimetype='image/jpg')
+        return send_file(darknet_path + '/predictions.jpg', mimetype='image/jpg')
         
